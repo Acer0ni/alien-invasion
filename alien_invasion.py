dd0ce43ba7de942cbbcc25465fdcb35930556d97
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-01-28 15:45:16 -0800

    added left and right movement for player ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """star the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             #redraw the screen during each pass through the loop.
 
@@ -31,6 +32,17 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
     def _update_screen(self):
         """updates images on the screen and flip the the new screen"""
         self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -14,6 +14,16 @@ class Ship:
         #start each new ship at the bottom of the center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #movement flag
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
