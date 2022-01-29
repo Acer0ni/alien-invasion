c5a8e9dc645abe461aaa702f67b669452cf2e5a7
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-01-28 16:13:46 -0800

    limited movement and added adjustable speed

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self,ai_game):
         """initiialize the ship and set its starting postion."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load the ship image  and get its rect.
@@ -14,15 +15,22 @@ class Ship:
         #start each new ship at the bottom of the center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
         #movement flag
         self.moving_right = False
         self.moving_left = False
     def update(self):
-        """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
