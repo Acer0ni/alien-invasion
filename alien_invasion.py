5048fbf18b8ef00a01eb5143d873ff2e52bcff51
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-01-31 13:28:21 -0800

    added ability to quit upon q press

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,6 +43,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.k_q:
+            sys.exit
     def _check_keyup_events(self,event):
         """responds to key releases"""
         if event.key == pygame.K_RIGHT:
