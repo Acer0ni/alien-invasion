f846344f72dae7cd638c6a8c039a6bb5488213bd
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-01-31 13:26:27 -0800

    refactored keyboard input

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,16 +33,22 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    #move the ship to the right
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+    def _check_keydown_events(self,event):
+        """responds to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            #move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+    def _check_keyup_events(self,event):
+        """responds to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     def _update_screen(self):
         """updates images on the screen and flip the the new screen"""
         self.screen.fill(self.settings.bg_color)
