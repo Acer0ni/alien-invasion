b26d6587449e2aca7a4e4327d0bccd76728c104a
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-02-01 16:12:19 -0800

    reset wave on clear

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,6 +82,10 @@ class AlienInvasion:
         #Check for any bullets that have hit aliens.
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets,self.aliens, True,True)
+        if not self.aliens:
+            #Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet
 
     def _update_screen(self):
         """updates images on the screen and flip the the new screen"""
