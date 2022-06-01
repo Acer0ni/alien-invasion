435f106a475df12b06e6519b105c3dc1c2f16d84
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-01 12:50:59 -0700

    made aliens worth more points in the later waves

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,7 +71,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
-
+            self.sb.prep_score()
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
@@ -125,7 +125,8 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
--- settings.py
+++ settings.py
@@ -24,6 +24,8 @@ class Settings:
 
         # how quickly the game speeds up
         self.speedup_scale = 1.1
+        # Howm quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -39,7 +41,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
