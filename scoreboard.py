f083ff0312c2639a59394ab52f684c0c2e28674d
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-01 13:41:48 -0700

    added highscores

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,20 @@ class Scoreboard:
 
         # Prepare  the initial score image.
         self.prep_score()
+        self.prep_high_score()
+
+    def prep_high_score(self):
+        """Turn the highscore into a rendered image."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -34,3 +48,10 @@ class Scoreboard:
     def show_score(self):
         """Draw score the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """Check to see if there's a new highscore."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
